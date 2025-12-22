95d9e6b0231556416ef81b39a7b098a81721fd75
EthanYang999 <ethan.zt.yang@gmail.com>
2025-12-23 00:10:24 +0800

    Initial Commit

---
+++ EarthLord/EarthLordApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthLordApp.swift
+//  EarthLord
+//
+//  Created by Ethan on 12/23/25.
+//
+
+import SwiftUI
+
+@main
+struct EarthLordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
